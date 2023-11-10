90295eb8d953b7c403f91b2685096275fd4ca75a
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-11-09 21:14:23 -0500

    code

---
+++ Coding Club/Team/Team.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Team {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int solved = 0;
+        int num = scanner.nextInt();
+        scanner.nextLine();
+        for(int i=0;i<num;i++){
+            String problem = scanner.nextLine();
+            int space=problem.indexOf(" ");
+            int Petya = Integer.parseInt(problem.substring(0,space));
+            String problemPtTwo=problem.substring(space+1);
+            int spaceTwo=problemPtTwo.indexOf(" ");
+            int Vasya = Integer.parseInt(problemPtTwo.substring(0,spaceTwo));
+            int Tonya = Integer.parseInt(problemPtTwo.substring(spaceTwo+1));
+            if ((Petya==1&&Vasya==1)||(Petya==1&&Tonya==1)||(Vasya==1&&Tonya==1)){
+                solved++;
+            }
+        }
+        System.out.println(solved);
+    }
+}
