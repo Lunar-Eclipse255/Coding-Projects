fc9ed886b364b02d03a899eeebee2147bd164f81
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-11-15 09:17:49 -0500

    code

---
+++ Coding Club/Next Round/Round.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Round {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int lowScore = scanner.nextInt();
+        System.out.println(num);
+        System.out.println(lowScore);
+        int[] scores = new int[n];
+    }
+}
