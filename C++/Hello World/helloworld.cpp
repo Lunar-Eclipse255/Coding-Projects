9950d86c5f0cd02d3c4cd88f6063984af8847ac6
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-11-17 20:56:13 -0500

    printing+variables

---
+++ C++/Hello World/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << "I am learning C++\n\n";
+  cout <<"Another Line"<<endl;
+  cout <<"Test\n";
+  int num=1;
+  cout<<num<<"\n";
+  cout<<"There is "<<num<<" apple\n";
+  int x,y,z;
+  x=y=z=5;
+  cout<<x<<y<<z<<"\n";
+  return 0;
+}
