a82f71dffe9383571d2cf8c6f8c3dffaf920a528
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-11-17 21:44:47 -0500

    addition

---
+++ C++/Addition/Addition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"Give a number: ";
+    cin >> x;
+    cout<<"Give another number: ";
+    cin>>y;
+    cout<<"Number one plus number two is "<<(x+y)<<"\n";
+    return 0;
+}
